db8e4319b2ac629869a7861e68e86c043643200e
Devon Wehrlin <dkwehrlin.99@gmail.com>
2020-07-23 17:52:03 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Devon"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Akron, OH"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Unholy Confessions"
+  end 
 end
