aa653d10a716bd16cba580c1ad63f09536dd16e2
Faith Gicheha <fwgicheh@gmail.com>
2020-07-28 19:50:11 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do 
+    "My name is Faith Gicheha." 
+  end 
+
+  get '/hometown' do    
+    "My hometown is Sacramento."
+  end  
+
+  get '/favorite-song' do
+     "My favorite song is Back to Sleep."
+  end 
+
 end
